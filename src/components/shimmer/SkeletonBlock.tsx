c76a83876717e1671a22371c676455a2188c127f
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 09:04:49 +0500

    diagonal glow shimmer effect designed

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -6,11 +6,12 @@
 // import styles from "./slowGlowShimmer.module.css";
 // import styles from "./dualLayerShimmer.module.css";
 // import styles from "./glowFadeShimmer.module.css";
-import styles from "./waveShimmer.module.css";
+// import styles from "./waveShimmer.module.css";
+import styles from "./diagonalGlowShimmer.module.css";
 
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return <div className={`${styles.waveShimmer} ${className}`} {...props} />;
+  return <div className={`${styles.diagonalGlow} ${className}`} {...props} />;
 };
 
 export default SkeletonBlock;
