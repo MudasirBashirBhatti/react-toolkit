db19e8449245aedfe9f446ac2dfb903474dfabdc
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 09:11:44 +0500

    flicker shimmer and blue shimmer added

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -7,11 +7,15 @@
 // import styles from "./dualLayerShimmer.module.css";
 // import styles from "./glowFadeShimmer.module.css";
 // import styles from "./waveShimmer.module.css";
-import styles from "./diagonalGlowShimmer.module.css";
+// import styles from "./diagonalGlowShimmer.module.css";
+// import styles from "./flickerShimmer.module.css";
+
+// gradient shimmers
+import styles from "./blueGradientShimmer.module.css";
 
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return <div className={`${styles.diagonalGlow} ${className}`} {...props} />;
+  return <div className={`${styles.blueShimmer} ${className}`} {...props} />;
 };
 
 export default SkeletonBlock;
