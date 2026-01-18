e94f72a46e261d49954f60d812ed045e8078dfed
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 08:49:29 +0500

    pulse shimmer effect designed

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -1,10 +1,9 @@
 // import styles from "./shimmerAnimation.module.css";
-import styles from "./diagonalShimmer.module.css";
+// import styles from "./diagonalShimmer.module.css";
+import styles from "./pulseShimmer.module.css";
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return (
-    <div className={`${styles.diagonalShimmer} ${className}`} {...props} />
-  );
+  return <div className={`${styles.pulseShimmer} ${className}`} {...props} />;
 };
 
 export default SkeletonBlock;
