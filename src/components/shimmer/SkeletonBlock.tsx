5e8d085ee47080915d75e3e85390e3cd0273fa61
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 09:19:32 +0500

    soft glow shimmer designed

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -9,14 +9,17 @@
 // import styles from "./waveShimmer.module.css";
 // import styles from "./diagonalGlowShimmer.module.css";
 // import styles from "./flickerShimmer.module.css";
+import styles from "./softGlowShimmer.module.css";
 
 // gradient shimmers
 // import styles from "./blueGradientShimmer.module.css";
-import styles from "./rainbowGradientShimmer.module.css";
+// import styles from "./rainbowGradientShimmer.module.css";
 
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return <div className={`${styles.rainbowShimmer} ${className}`} {...props} />;
+  return (
+    <div className={`${styles.softGlowShimmer} ${className}`} {...props} />
+  );
 };
 
 export default SkeletonBlock;
