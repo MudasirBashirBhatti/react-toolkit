41034d099c3cbab54c3231b3c6676f5cd2b53e00
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 09:02:41 +0500

    wave shimmer effect created

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -5,13 +5,12 @@
 // import styles from "./multiShimmer.module.css";
 // import styles from "./slowGlowShimmer.module.css";
 // import styles from "./dualLayerShimmer.module.css";
-import styles from "./glowFadeShimmer.module.css";
+// import styles from "./glowFadeShimmer.module.css";
+import styles from "./waveShimmer.module.css";
 
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return (
-    <div className={`${styles.glowFadeShimmer} ${className}`} {...props} />
-  );
+  return <div className={`${styles.waveShimmer} ${className}`} {...props} />;
 };
 
 export default SkeletonBlock;
