6aa118ed8793bb66367d26f8abf569bfbb219d48
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-18 08:52:05 +0500

    vertical shimmerDesigned

--- src/components/shimmer/SkeletonBlock.tsx
+++ src/components/shimmer/SkeletonBlock.tsx
@@ -1,9 +1,13 @@
 // import styles from "./shimmerAnimation.module.css";
 // import styles from "./diagonalShimmer.module.css";
-import styles from "./pulseShimmer.module.css";
+// import styles from "./pulseShimmer.module.css";
+import styles from "./verticalShimmer.module.css";
+
 type SkeletonBlockProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 const SkeletonBlock = ({ className, ...props }: SkeletonBlockProps) => {
-  return <div className={`${styles.pulseShimmer} ${className}`} {...props} />;
+  return (
+    <div className={`${styles.verticalShimmer} ${className}`} {...props} />
+  );
 };
 
 export default SkeletonBlock;
