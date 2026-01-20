f965b37d090b75af04d21b14fa547f2d45913b3e
Mudasir <mudasir.bhatti358@gmail.com>
2026-01-20 23:46:11 +0500

    home page made clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import SkeletonComponent from "@/components/shimmer/SkeletonComponent/SkeletonComponent";
 import styles from "./page.module.css";
-import StatusPill from "@/components/tags/StatusPill/StatusPill";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      welcome to ui library <SkeletonComponent />
-    </div>
-  );
+  return <div className={styles.page}>welcome to ui library</div>;
 }
